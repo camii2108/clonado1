06c62aa67dc9b8aa618be796dc7ca8baf211682d
Sebastian Faliani <nedase@gmail.com>
2022-11-15 23:18:56 -0300

    funciones

--- cami1.js
+++ cami1.js
@@ -1,4 +1,9 @@
-function multiplicar (n1,n2){
-    return n1 + n2;
+function sumar(n1, n2) {
+      return n1 + n2;
 }
-console.log(multiplicar(2,2))
+console.log(multiplicar(2, 2));
+
+function multiplicar(n1, n2) {
+      return n1 * n2;
+}
+console.log(multiplicar(2, 2));
