24c9cf662b4e5814afb7a77e33856f15e5a92b94
Sebastian Faliani <nedase@gmail.com>
2022-11-15 23:44:51 -0300

    hola

--- cami1.js
+++ cami1.js
@@ -1,20 +1,21 @@
 function sumar(n1, n2) {
       return n1 + n2;
 }
-console.log(multiplicar(2,2))
+console.log(multiplicar(2, 2));
 
-function restar (n1,n2){
-    return n1 - n2;
+function restar(n1, n2) {
+      return n1 - n2;
 }
-console.log(multiplicar(2,2))
-console.log(restar(2,2))
-function dividir (n1,n2){
-    return n1/n2;
+console.log(multiplicar(2, 2));
+console.log(restar(2, 2));
+function dividir(n1, n2) {
+      return n1 / n2;
 }
-console.log(dividir(2,2));
+console.log(dividir(2, 2));
 console.log(multiplicar(2, 2));
 
 function multiplicar(n1, n2) {
       return n1 * n2;
 }
 console.log(multiplicar(2, 2));
+let pp = 0;
