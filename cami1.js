854ceb8f4e77e7c623bdbc5bbf6180f6409a800a
camila2108 <gutierrezcamilamilagros02@gmail.com>
2022-11-15 23:25:47 -0300

    modificando funciones

--- cami1.js
+++ cami1.js
@@ -2,3 +2,9 @@ function multiplicar (n1,n2){
     return n1 + n2;
 }
 console.log(multiplicar(2,2))
+
+function restar (n1,n2){
+    return n1 - n2;
+}
+console.log(multiplicar(2,2))
+console.log(restar(2,2))
